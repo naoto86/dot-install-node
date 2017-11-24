21fce225c71885f1d0f9a1296ff734f687a4b90e
naoto86 <memento.mori.1986@gmail.com>
2017-11-24 13:47:10 +0900

    第六回目
    Signed-off-by: naoto86 <memento.mori.1986@gmail.com>

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,12 @@
 var http = require('http');
+var settings = require('./settings')
 var server = http.createServer();
-
+console.log(settings);
 server.on('request', function(req, res) {
   res.writeHead(200,{'Content-Type':'text/plain'});
   res.write('hello world!!!');
   res.end();
 });
 // server.listen(1337,'192.168.33.72');
-server.listen(8080,'127.0.0.1')
+server.listen(settings.port, settings.host);
 console.log("server listening ...")
