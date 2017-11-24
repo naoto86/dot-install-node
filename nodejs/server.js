04b4399a2d2032d1fda5fa3dcda32392eac523e7
naoto86 <memento.mori.1986@gmail.com>
2017-11-24 18:32:45 +0900

    第十二回目
    Signed-off-by: naoto86 <memento.mori.1986@gmail.com>

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,22 +1,35 @@
 var http = require('http');
 var fs = require('fs')
 var ejs = require('ejs')
+var qs = require('querystring')
 var settings = require('./settings')
 var server = http.createServer();
 
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8')
-var n = 0
-server.on('request', function(req, res) {
-  n++;
-  var data = ejs.render(template, {
-    title:"hello",
-    content:"<strong>World!</strong>",
-    n: n
-  });
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8')
+var posts = []
 
+function renderForm(posts, res){
+  var data = ejs.render(template,{
+    posts: posts
+  })
   res.writeHead(200,{'Content-Type':'text/html'});
   res.write(data);
   res.end();
+}
+
+server.on('request', function(req, res) {
+  if (req.method === 'POST' ){
+    req.data = "";
+    req.on("readable", function(){
+      req.data += read.read();
+    })
+    req.on("end", function(){
+
+    })
+  }else{
+    renderForm(posts, res)
+  }
+
 });
 // server.listen(1337,'192.168.33.72');
 server.listen(settings.port, settings.host);
