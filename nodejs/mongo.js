1b4d20c8e4539a2006fe3a0916bbaab3d032451c
naoto86 <memento.mori.1986@gmail.com>
2017-11-27 14:59:58 +0900

    第十五回

--- nodejs/mongo.js
+++ nodejs/mongo.js
@@ -3,5 +3,14 @@ var settings = require('./settings');
 MongoClient.connect("mongodb://localhost/"+settings.db, function (err, db) {
   if(err){return console.dir(err);}
   console.log("conected to db")
-
+  db.collection("users",function(err, collection){
+    var docs =[
+      {name:'taguchi',score:40},
+      {name:'fkoji',score:80},
+      {name:'dotinstall',score:60},
+    ]
+    collection.insert(docs, function(err, result){
+      console.dir(result)
+    })
+  })
 })
